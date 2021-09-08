546ce4f2ad7550f102ba76524bdaa3057f5431f9
Huy Hieu <huyhieugckm@gmail.com>
2021-09-08 11:16:12 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Cshook from "./Cshook";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const arrayList = ["Huyen","Long","Manh","Hieu"];
+  const [insertName,onSubmit,name,arrData] = Cshook(arrayList)
+return (
+  <div>
+     <div>
+         学生一覧：[Huyen,Long,Manh,Hieu]
+     </div>
+      <div>
+          追加する名前を入力してください。
+      </div>
+      <input onChange={insertName}/>
+      <button onClick={()=>onSubmit()}>
+          確定
+      </button>
+      <div>
+          追加する名前:{name}
+      </div>
+      <div>
+          新しい一覧：{arrData.map((i,key)=>{
+          if(key ==0)
+          {
+              return(
+                  <span>
+                   [{i},
+               </span>
+              )} else if(key == arrData.length -1){
+              return(
+                  <span>
+                   {i}]
+               </span>
+              )
+          } else{
+              return(
+                  <span>
+                       {i},
+                   </span>
+              )
+          }
+      } )}
+      </div>
+  </div>
+);
 }
 
 export default App;
